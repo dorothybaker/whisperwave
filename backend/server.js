76d88bdd8e70bd47cdcca91eab1ce76bb657fda8
Dorothy Baker <tbakerdorothy@gmail.com>
2024-02-16 07:26:27 +0300

    modifications

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 config();
 
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,13 +27,15 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("This is the root route!");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongo();
